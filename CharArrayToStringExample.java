858266ae263899c210f04b6f2f0d48151a1654ab
KusumSingh1 <kusumsingh0808@gmail.com>
2021-10-19 11:53:29 +0530

    test java

---
+++ CharArrayToStringExample.java
@@ -0,0 +1,7 @@
+public class CharArrayToStringExample{
+        public static void main(String args[]){
+      char[] ch={'w','e','l','c','o','m','e','t','o','j','a','v','a','p','o','i','n','t'};
+      String string=new String(ch);
+      System.out.println(string);
+}
+}
