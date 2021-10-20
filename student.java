8d4e014aa6610ba8ec34ca2cc43654979b7e8961
KusumSingh1 <kusumsingh0808@gmail.com>
2021-10-20 14:38:34 +0530

    student

---
+++ student.java
@@ -0,0 +1,27 @@
+public class Student
+
+String          Name
+int             ID
+int             marks
+string          email ID
+long/string     university roll no.
+char            gender
+long/string     mobile no.
+string          department
+int             age
+char            category
+string          address
+int             D.O.B
+float           fee structure
+float           percentage
+char            grade
+
+
+
+public String displayName(){
+  string studentName="kusum";
+  System.out.println("My Name is " +studentName);
+  return studentName;
+}
+ 
+ 
